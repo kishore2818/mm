5a5a69c0ba0a56338572e19f18dec3b2e34dd41f
Kishore Abinash <kishoreabinash2005@gmail.com>
2026-07-15 22:21:47 +0530

    Clean up unwanted root folders (.next, datas)

--- .next/dev/static/chunks/src_app_layout_tsx_007e4b2._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_app_globals_162hn9o.css","static/chunks/node_modules_next_1iyq0l7._.js","static/chunks/node_modules_motion-dom_dist_es_14228j4._.js","static/chunks/node_modules_framer-motion_dist_es_1_a7n4k._.js","static/chunks/node_modules_1s_u-if._.js","static/chunks/src_components_layout_Navbar_tsx_1z_c3lx._.js"],
-    source: "dynamic"
-});
